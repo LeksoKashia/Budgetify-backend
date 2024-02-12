6984a397be11cab25980b5cb170e47ca98c85aa1
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-12 21:48:23 +0400

    modified user model

--- src/main/java/exadel/budgetify/Budgetify/model/User.java
+++ src/main/java/exadel/budgetify/Budgetify/model/User.java
@@ -4,23 +4,33 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-public class User  implements Serializable {
+public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
+
     private String name;
     private String email;
     private String password;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Account> accounts = new ArrayList<>();
+
     public User() {
     }
+
     public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
     }
+
 
     public Long getId() {
         return id;
@@ -52,6 +62,14 @@ public class User  implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
     }
 
     @Override
