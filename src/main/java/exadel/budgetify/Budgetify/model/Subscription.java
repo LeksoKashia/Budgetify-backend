e0e44acea95c9ab55a0329ddf4373380185bffc2
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2024-02-12 21:43:40 +0400

    created subscription model

---
+++ src/main/java/exadel/budgetify/Budgetify/model/Subscription.java
@@ -0,0 +1,111 @@
+package exadel.budgetify.Budgetify.model;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String categories;
+
+    private float amount;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "start_date", nullable = false, updatable = false)
+    private Date startDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "end_date", nullable = false, updatable = false)
+    private Date endDate;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    public Subscription() {
+    }
+
+    public Subscription(String title, String categories, float amount, Date startDate, Date endDate,
+                        String description, Account account) {
+        this.title = title;
+        this.categories = categories;
+        this.amount = amount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.account = account;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
